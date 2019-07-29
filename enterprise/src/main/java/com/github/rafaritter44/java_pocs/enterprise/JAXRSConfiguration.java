1b31243cf24caebe02efe402f076544fead5a76d
Rafael Ritter <rafaritter44@hotmail.com>
2019-07-28 21:58:46 -0300

    Implement JEE Application

---
+++ enterprise/src/main/java/com/github/rafaritter44/java_pocs/enterprise/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package com.github.rafaritter44.java_pocs.enterprise;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JAXRSConfiguration extends Application {
+
+}
---
+++ enterprise/src/main/java/com/github/rafaritter44/java_pocs/enterprise/resource/HomeworkAssignmentResource.java
@@ -0,0 +1,49 @@
+package com.github.rafaritter44.java_pocs.enterprise.resource;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.github.rafaritter44.java_pocs.enterprise.dao.HomeworkAssignmentDAO;
+import com.github.rafaritter44.java_pocs.enterprise.entity.HomeworkAssignment;
+
+@Stateless
+@Path("/homework-assignment")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class HomeworkAssignmentResource {
+	
+	@EJB
+	private HomeworkAssignmentDAO homeworkAssignmentDAO;
+	
+	@GET
+	public Response findAll() {
+		return Response.ok(homeworkAssignmentDAO.findAll()).build();
+	}
+	
+	@GET
+	@Path("/{id}")
+	public Response findById(@PathParam("id") Long id) {
+		return Response.ok(homeworkAssignmentDAO.findById(id)).build();
+	}
+	
+	@POST
+	public Response save(HomeworkAssignment homeworkAssignment) {
+		return Response.ok(homeworkAssignmentDAO.save(homeworkAssignment)).build();
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	public Response remove(@PathParam("id") Long id) {
+		return Response.ok(homeworkAssignmentDAO.removeById(id)).build();
+	}
+	
+}
