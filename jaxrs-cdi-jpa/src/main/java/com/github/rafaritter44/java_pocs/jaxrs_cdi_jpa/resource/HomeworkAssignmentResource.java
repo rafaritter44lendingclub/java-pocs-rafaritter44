da8ed7de8f4164f9997a4b9bd5938032cac09736
rafaritter44 <31143678+rafaritter44@users.noreply.github.com>
2019-10-07 23:12:59 -0300

    Update HomeworkAssignmentResource.java

--- jaxrs-cdi-jpa/src/main/java/com/github/rafaritter44/java_pocs/jaxrs_cdi_jpa/resource/HomeworkAssignmentResource.java
+++ jaxrs-cdi-jpa/src/main/java/com/github/rafaritter44/java_pocs/jaxrs_cdi_jpa/resource/HomeworkAssignmentResource.java
@@ -40,7 +40,7 @@ public class HomeworkAssignmentResource {
 	
 	@DELETE
 	@Path("/{id}")
-	public Response remove(@PathParam("id") Long id) {
+	public Response removeById(@PathParam("id") Long id) {
 		return Response.ok(homeworkAssignmentDAO.removeById(id)).build();
 	}
 	
