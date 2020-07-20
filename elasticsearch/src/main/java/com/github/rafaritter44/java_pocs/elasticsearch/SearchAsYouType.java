6f7ed2c81bd4f2e031856f238dd82d63449c4859
Rafael Ritter <rafaritter44@hotmail.com>
2020-07-19 23:35:09 -0300

    Fix spacing

--- elasticsearch/src/main/java/com/github/rafaritter44/java_pocs/elasticsearch/SearchAsYouType.java
+++ elasticsearch/src/main/java/com/github/rafaritter44/java_pocs/elasticsearch/SearchAsYouType.java
@@ -22,66 +22,66 @@ public class SearchAsYouType extends JPanel implements KeyListener {
 	
 	private final MovieService service;
 	private final StringBuilder userInput;
-    private final JTextField textField;
-    private final JTextArea textArea;
+	private final JTextField textField;
+	private final JTextArea textArea;
 
-    public SearchAsYouType() {
-        super(new GridBagLayout());
-        
-        final RestHighLevelClient client = new RestHighLevelClient(
+	public SearchAsYouType() {
+		super(new GridBagLayout());
+		
+		final RestHighLevelClient client = new RestHighLevelClient(
 				RestClient.builder(
 						new HttpHost("localhost", 9200, "http")));
-        service = new MovieService(client);
-        
-        userInput = new StringBuilder();
+		service = new MovieService(client);
+		
+		userInput = new StringBuilder();
 
-        textField = new JTextField(20);
-        textField.addKeyListener(this);
+		textField = new JTextField(20);
+		textField.addKeyListener(this);
 
-        textArea = new JTextArea(5, 20);
-        textArea.setEditable(false);
-        final JScrollPane scrollPane = new JScrollPane(textArea);
+		textArea = new JTextArea(5, 20);
+		textArea.setEditable(false);
+		final JScrollPane scrollPane = new JScrollPane(textArea);
 
-        // Add Components to this panel.
-        final GridBagConstraints c = new GridBagConstraints();
-        c.gridwidth = GridBagConstraints.REMAINDER;
+		// Add Components to this panel.
+		final GridBagConstraints c = new GridBagConstraints();
+		c.gridwidth = GridBagConstraints.REMAINDER;
 
-        c.fill = GridBagConstraints.HORIZONTAL;
-        add(textField, c);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		add(textField, c);
 
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1.0;
-        c.weighty = 1.0;
-        add(scrollPane, c);
-    }
+		c.fill = GridBagConstraints.BOTH;
+		c.weightx = 1.0;
+		c.weighty = 1.0;
+		add(scrollPane, c);
+	}
 
-    /**
-     * Create the GUI and show it. For thread safety,
-     * this method should be invoked from the
-     * event dispatch thread.
-     */
-    private static void createAndShowGUI() {
-        // Create and set up the window.
-        JFrame frame = new JFrame("Search As You Type");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	/**
+	 * Create the GUI and show it. For thread safety,
+	 * this method should be invoked from the
+	 * event dispatch thread.
+	 */
+	private static void createAndShowGUI() {
+		// Create and set up the window.
+		final JFrame frame = new JFrame("Search As You Type");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Add contents to the window.
-        frame.add(new SearchAsYouType());
+		// Add contents to the window.
+		frame.add(new SearchAsYouType());
 
-        // Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
 
-    public static void main(String[] args) {
-        // Schedule a job for the event dispatch thread:
-        // creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
+	public static void main(String[] args) {
+		// Schedule a job for the event dispatch thread:
+		// creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
 
 	@Override
 	public void keyTyped(final KeyEvent e) {
