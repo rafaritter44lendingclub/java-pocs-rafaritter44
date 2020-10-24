09fe743992773258af226be3c6918e8900670d9d
Rafael Ritter <rafaritter44@hotmail.com>
2020-10-24 09:32:49 -0300

    Revert "Implement Embedded Keycloak Spring Boot server"
    This reverts commit 5873f6e7dd00649085f4f9df0b8d61eddb3fe194.

--- spring/security/authorization-server/src/main/java/com/github/rafaritter44/java_pocs/spring/security/authorization_server/config/RegularJsonConfigProviderFactory.java
+++
@@ -1,5 +0,0 @@
-package com.github.rafaritter44.java_pocs.spring.security.authorization_server.config;
-
-import org.keycloak.services.util.JsonConfigProviderFactory;
-
-public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {}
