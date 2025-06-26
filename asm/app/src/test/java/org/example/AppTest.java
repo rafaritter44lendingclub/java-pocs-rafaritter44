b07657adbe05a8cbb593da21f9f58aa4bccdb359
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-06-26 12:30:09 -0300

    Test print injector 2

--- asm/app/src/test/java/org/example/AppTest.java
+++ asm/app/src/test/java/org/example/AppTest.java
@@ -1,10 +1,26 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Path;
+
+import org.example.printinjector.PrintInjector2;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        assertNotNull("Not null", "String should not be null.");
+    @Test void test() throws Exception {
+        PrintInjector2.main(new String[]{});
+        
+        Path path = Path.of("build/classes/java/main/");
+        URL classDir = path.toUri().toURL();
+        try (URLClassLoader loader = new URLClassLoader(new URL[]{classDir}, null)) { // null = no parent classloader
+            Class<?> greeterClass = loader.loadClass("org.example.printinjector.Greeter");
+
+            Object greeter = greeterClass.getDeclaredConstructor().newInstance();
+            Method greet = greeterClass.getMethod("greet");
+            greet.invoke(greeter);
+        }
     }
 }
